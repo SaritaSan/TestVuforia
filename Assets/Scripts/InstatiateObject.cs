ab3ed0db42b9573744a3aaf5da1013cb30317e72
SaritaSan <156473953+SaritaSan@users.noreply.github.com>
2025-05-14 20:39:38 -0600

    Animaciones, boton, musica

---
+++ Assets/Scripts/InstatiateObject.cs
@@ -0,0 +1,12 @@
+using UnityEngine;
+
+public class InstatiateObject : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject objectToInstantiate;
+
+    public void Instantiate(Transform target)
+    {
+        Instantiate(objectToInstantiate, target.position,Quaternion.identity);
+    }
+}
