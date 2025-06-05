191b5ac816140675f77d32675a8342998a27e52d
SaritaSan <156473953+SaritaSan@users.noreply.github.com>
2025-06-04 21:20:44 -0600

    Avance primer parcial
    Lanes y Assets

--- Assets/Scripts/NotesChecker.cs
+++ Assets/Scripts/NotesChecker.cs
@@ -10,9 +10,7 @@ public class NotesChecker : MonoBehaviour
     private UnityEvent onCorrectNote;
     [SerializeField]
     private UnityEvent onFailNote;
-
     private List<GameObject> notes = new List<GameObject>();
-
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.CompareTag("Note"))
@@ -20,7 +18,6 @@ public class NotesChecker : MonoBehaviour
             notes.Add(collision.gameObject);
         }
     }
-
     private void OnTriggerExit2D(Collider2D collision)
     {
         if (collision.CompareTag("Note"))
@@ -28,24 +25,31 @@ public class NotesChecker : MonoBehaviour
             notes.Remove(collision.gameObject);
         }
     }
-
     public void DestroyNotes()
     {
-        if (notes.Count > 0)
+        onButtonPressed?.Invoke();
+        GameObject noteToDestroy = null;
+        int indexToRemove = -1;
+        for (int i = 0; i < notes.Count; i++)
+        {
+            if (notes[i] != null)
+            {
+                noteToDestroy = notes[i];
+                indexToRemove = i;
+                break;
+            }
+        }
+
+        if (noteToDestroy != null)
         {
             onCorrectNote?.Invoke();
+            notes.RemoveAt(indexToRemove);
+            Destroy(noteToDestroy);
         }
         else
         {
             onFailNote?.Invoke();
         }
-        onButtonPressed?.Invoke();
-        while (notes.Count > 0)
-        {
-            GameObject note = notes[0];
-            notes.RemoveAt(0);
-            Destroy(note);
-        }
-        notes.Clear();
     }
+
 }
